ffa0af80558848bbfadfc7130103c2335d57c3d4
Ryan Lopopolo <rjl@hyperbo.la>
2022-04-25 18:30:01 -0700

    Ensure macros work when builtin types like u8, bool, and str are shadowed
    Using type aliases for builtin types exposed in imp module.
    
    Closes #16.

--- src/imp.rs
+++ src/imp.rs
@@ -26,6 +26,17 @@ pub const fn contains_nul(slice: &[u8]) -> bool {
     matches!(find(slice, 0), Some(_))
 }
 
+pub mod types {
+    // Type alias for `u8`.
+    pub type Bool = bool;
+    // Type alias for `u8`.
+    pub type U8 = u8;
+    // Type alias for `usize`.
+    pub type Usize = usize;
+    // Type alias for `str`.
+    pub type Str = str;
+}
+
 #[cfg(test)]
 mod tests {
     use super::{contains_nul, find, is_cstr};
