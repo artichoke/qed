7fe04269c6325514c3a74ed4984991d21700fb0a
artichoke-ci <ci@artichokeruby.org>
2025-11-03 02:47:19 +0000

    deploy: 0361922a8b7794f75c519d4b8f2e611256bd71cd

---
+++ crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["qed"];
+//{"start":21,"fragment_lengths":[5]}
---
+++ src-files.js
@@ -0,0 +1,2 @@
+createSrcSidebar('[["qed",["",[],["imp.rs","lib.rs"]]]]');
+//{"start":19,"fragment_lengths":[35]}
